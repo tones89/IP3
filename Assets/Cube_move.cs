2e5fea7b295a6fbbb644792469c5b3ca75d48dc0
Anthony Denovan <adenov200@caledonian.ac.uk>
2013-01-17 11:53:14 +0000

    Added a starter class for the game

---
+++ Assets/Cube_move.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class Cube_move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		transform.Rotate(0,5*Time.deltaTime,0);
+		
+	}
+}
